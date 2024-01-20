12b5c754efc6354248ceb4b67254c8db7d0524c4
Jhones Jhonatas <jhones.jhonatas@outlook.com.br>
2024-01-20 19:41:22 -0300

    feat/createDatabase :art:

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express, { NextFunction, Request, Response } from 'express'
 import cors from 'cors'
+import 'dotenv/config'
 import { AppError } from './errors/AppError'
 import { routes } from './routes'
 
